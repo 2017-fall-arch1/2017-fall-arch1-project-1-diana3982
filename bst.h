44a7a567dc83ef4bb2bf568681113d2ec7292662
diana3982 <31858218+diana3982@users.noreply.github.com>
2017-10-20 18:46:38 -0600

    edit struct

--- bst.h
+++ bst.h
@@ -1,11 +1,15 @@
 #ifndef bst_include
 #define bst_include
+//#include "bst.c"
+
 
 typedef struct Node{
   char *name;
-  struct Node *left;
-  struct Node *right;
-}
-  Node;
+  struct Node* left;
+  struct Node* right;
+}Node;
+
+struct Node* Insert(Node* root, char* name);
 
 #endif
+
