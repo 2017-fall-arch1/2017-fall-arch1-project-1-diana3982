4d44c78c389648223edfded34e35ae326b74f7e2
diana3982 <31858218+diana3982@users.noreply.github.com>
2017-10-23 00:17:30 -0600

    moved typedef back over to header

--- Lab1/bst.h
+++ Lab1/bst.h
@@ -1,6 +1,12 @@
 #ifndef bst_include
 #define bst_include
 
+typedef struct Node{
+  char* name;
+  struct Node* left;
+  struct Node* right;
+}Node;
+
 struct Node* NewEmployee(char* name);
 struct Node* Insert(struct Node* root, char* name);
 struct Node* Delete(struct Node* root, char* name);
