5c843fcf2e5617ea03eaf08b483ab8cf64158965
diana3982 <31858218+diana3982@users.noreply.github.com>
2017-10-22 22:01:43 -0600

    add NewEmployee in attempt to fix linker error

--- Lab1/bst.h
+++ Lab1/bst.h
@@ -1,6 +1,7 @@
 #ifndef bst_include
 #define bst_include
 
+struct Node* NewEmployee(char* name);
 struct Node* Insert(struct Node* root, char* name);
 struct Node* Delete(struct Node* root, char* name);
 void Traverse(struct Node* root);
